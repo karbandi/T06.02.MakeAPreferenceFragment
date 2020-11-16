714c37ddcdb56b9c56d7bdb7d62cface46531b14
ar.karbandi@gmail.com <ar.karbandi@gmail.com>
2020-11-16 16:46:34 +0330

    MakeAPreferenceFragment

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -3,12 +3,7 @@ package android.example.com.visualizerpreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat{
-
-    public SettingsFragment() {
-        SettingsFragment.instantiate(getContext(),"SettingsFragment");
-    }
-
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         addPreferencesFromResource(R.xml.pref_visualizer);
